2bc5bc674a7732ad4de8481c6467e60bf9cde99f
Brun096 <Brunoh96@outlook.it>
2021-11-22 14:31:36 +0100

    Ho creato un programma che mostra se la parola è palindromo

---
+++ src/org/generation/italy/strings/VerificaPalindrome.java
@@ -0,0 +1,34 @@
+package org.generation.italy.strings;
+
+import java.util.Scanner;
+
+public class VerificaPalindrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scan=new Scanner(System.in);
+		String word;
+		String s="";
+		int sum=0;
+		
+		System.out.print("Inserisci una parola: ");
+		word=scan.nextLine();
+		
+		for(int i=word.length()-1 ;i>=0; i--) {
+			char c=word.charAt(i);
+			
+			s+=c;
+			sum++;
+			
+		}
+		if(word.equals(s)) {
+			System.out.println("La parola è palindromo, e ha "+sum+" lettere");
+		}else {
+			System.out.println("La parola non è palindromo, e ha "+sum+" lettere");
+		}
+		
+		scan.close();
+	}
+
+}
